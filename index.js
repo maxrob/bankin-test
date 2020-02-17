5d99a9a0f201ce317ef7b2ddffa4cf70a24cb981
Maxime Robin <maxime@liberkeys.com>
2020-02-17 09:53:00 +0100

    Improve variable recovery

--- index.js
+++ index.js
@@ -5,26 +5,25 @@ const REFRESH_TOKEN = "oa_sand_gHJC17S9MK-pW9K4JlZCea7yx9HRAn9rfc-S89-pT_0";
 
 const get_revolut_accounts = async () => {
   const jwt_token = revolut.get_jwt_token(CLIENT_ID);
-  const access_token_req = await revolut.get_access_token(
+  var {status, data: {access_token: access_token}} = await revolut.get_access_token(
     REFRESH_TOKEN,
     CLIENT_ID,
     jwt_token
   );
-  if (access_token_req.status != 200) {
+  if (status != 200) {
     throw new Error(
       "Une erreur est survenue durant la récupération de l'access token"
     );
   }
-  const access_token = access_token_req.data.access_token;
 
-  const accounts_req = await revolut.get_accounts(access_token);
-  if (accounts_req.status != 200) {
+  var {status, data: accounts} = await revolut.get_accounts(access_token);
+  if (status != 200) {
     throw new Error(
       "Une erreur est survenue durant la récupération de la liste des comptes"
     );
   }
 
-  return accounts_req.data;
+  return accounts;
 };
 
 get_revolut_accounts()
